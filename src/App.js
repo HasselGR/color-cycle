e7162487acd9bfa724f3e792d0d98202c7de055d
unknown <gamboadavid1998@gmail.com>
2022-07-27 14:41:30 -0400

    Adding toggle start pause button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,101 @@
-import { useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Input } from './components/input.component';
 import './App.css';
 
 function App() {
+  const intervalRef = useRef(null);
+
   const [colors, setColors] =useState({
     red:'',
     green:'',
     blue:'',
+    incrementRed:'',
+    incrementGreen:'',
+    incrementBlue:'',
   })
   const [color, setColor] = useState('')
+  const [time, setTime] =useState('');
+  const [pause, setPause] = useState(true)
+  const [intervalID, setIntervalID]=useState(0);
+
+ // useEffect(() => {
+  //   if (!pause) {
+  //     if (!intervalRef.current) {
+  //       intervalRef.current = setInterval(() => {
+  //         const newRed = parseInt(colors.red, 16) + parseInt(colors.incrementRed, 16)
+  //         const newBlue = parseInt(colors.blue, 16) + parseInt(colors.incrementBlue, 16)
+  //         const newGreen = parseInt(colors.green, 16) + parseInt(colors.incrementGreen, 16)
+  //         setColors(oldColors => ({
+  //           ...oldColors,
+  //             red: newRed.toString(16),
+  //             blue: newBlue.toString(16),
+  //             green: newGreen.toString(16),
+  //         }))
+  //       }, 250)
+  //       setColor(`#${colors.red}${colors.green}${colors.blue}`)
+  //     }
+  //   } else {
+  //     if (intervalRef.current) {
+  //       clearInterval(intervalRef.current)
+  //       intervalRef.current = null
+  //     }
+  //   }
+  // }, [pause, colors]) 
+ //estudiar mejor los array de dependencias
+
+
 
 
   const handleChange = (event) => {
-    
-    setColors(oldColors => ({
-      ...oldColors,
-      [event.target.name]: event.target.value,
-    }))
-    console.log(colors);
+    if(/^[0-9a-f]+$/.test(event.target.value) && colors[event.target.name].length < 2){
+      setColors(oldColors => ({
+        ...oldColors,
+        [event.target.name]: event.target.value,
+      }))
+    }else{
+      alert('Invalid Character or Length, We are only able to accept hexadecimal symbols')
+      return;
+    }
+  }
+
+
+
+
+
+  const handleStart = () =>{
+    if(intervalID){ 
+      clearInterval(intervalID);
+      setIntervalID(0);
+      console.log('the colors ended like this:', colors)
+      return;
+    }
+
+    const newInterval = setInterval(() => {
+          setColors(oldColors => ({
+            ...oldColors,
+              red: (parseInt(oldColors.red, 16) + parseInt(oldColors.incrementRed, 16)).toString(16),
+              blue: (parseInt(oldColors.blue, 16) + parseInt(oldColors.incrementBlue, 16)).toString(16),
+              green: (parseInt(oldColors.green, 16) + parseInt(oldColors.incrementGreen, 16)).toString(16),
+          }))
+          // console.log(colors.red, colors.green, colors.blue)
+          // setColor(`#${colors.red}${colors.green}${colors.blue}`)
+          // console.log(color)
+        }, 1000)
+    setIntervalID(newInterval);
+  }
+  
+
+ 
+
+  const handleNumberChange= (event) =>{
+    setTime(event.target.value)
   }
 
   const handleSubmit= (event) =>{
     event.preventDefault()
     setColor(`#${colors.red}${colors.green}${colors.blue}`)
   }
+
   return (
     <div>
       <div className="page-container">
@@ -38,23 +110,25 @@ function App() {
       </div>
       <div>
         <form onSubmit={handleSubmit} className="inputs-container">
-          <h1>Input for colors:</h1>
+            <h1>Input for colors:</h1>  
           <div className='break'/>
-         {/* <input name='red' placeholder='Red' onChange={handleChange}/>
-         <input name='green' placeholder='Green' onChange={handleChange}/>
-         <input name='blue' placeholder='Blue' onChange={handleChange}/> */}
-         <Input color="red" change={handleChange}/>
-         <Input color="green" change={handleChange}/>
-         <Input color="blue"  change={handleChange}/>
-         <div className='break'/>
-          <h1>Color Increment:</h1>
+            <Input color="red" state={colors.red} change={handleChange}/>
+            <Input color="green" state={colors.green} change={handleChange}/>
+            <Input color="blue"  state={colors.blue} change={handleChange}/>
           <div className='break'/>
-         <Input color="Red"/>
-         <Input color="Green"/>
-         <Input color="Blue"/>
-         <div className='break'/>
-         <button className="button"type='submit'>Submit Colors</button>
+            <h1>Color Increment:</h1>
+          <div className='break'/>
+            <Input color="incrementRed" state={colors.incrementRed} change={handleChange}/>
+            <Input color="incrementGreen" state={colors.incrementGreen} change={handleChange}/>
+            <Input color="incrementBlue" state={colors.incrementBlue} change={handleChange}/>
+          <div className='break'/>
+            <button className="button"type='submit'>Submit Colors</button>
+          <div className='break'/>
+            <h1>Change Interval:</h1>
+          <div className='break'/>
         </form>
+            <Input color="interval" state={time} change={handleNumberChange}/>
+            <button className='button' onClick={handleStart}>{intervalID===0? 'Start':'Pause' }</button>
       </div>
     </div>
   );
--- src/components/input.component.jsx
+++ src/components/input.component.jsx
@@ -1,8 +1,8 @@
 import './input.styles.css';
 
 
-export const Input = ({color, change}) => {
+export const Input = ({color, change, state}) => {
     return(
-        <input name={color} placeholder={color} onChange={change}></input>
+        <input name={color}  type={color==='time'? 'number': ''} value={state} placeholder={color} onChange={change}></input>
     )
 }
